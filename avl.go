ffb894fd857845d83e4dc9da429d8a03a61b7345
Caleb Rackliffe <calebrackliffe@gmail.com>
2014-04-11 23:22:13 -0700

    fiddling with a basic insert algorithm, an in-order traversal, and some visitor pattern stuff

--- avl.go
+++ avl.go
@@ -2,18 +2,50 @@ package avl
 
 type Node struct {
 	Left *Node
-	Key int
-	Value string
+	Value int
 	Right *Node
 }
 
-func (*Node) Insert(value int) {
+type Visitor interface {
+	Visit(node *Node)
 }
 
-func (*Node) Remove(value int) bool {
-	return false
+func (node *Node) Insert(value int) {
+	if value >= node.Value {
+		if node.Right != nil {
+			node.Right.Insert(value)
+		} else {
+			node.Right = &Node{nil, value, nil}
+		}
+	} else {
+		if node.Left != nil {
+			node.Left.Insert(value)
+		} else {
+			node.Left = &Node{nil, value, nil}
+		}
+	}
 }
 
-func (*Node) Find(value int) string {
-	return nil
+func (node *Node) InOrder(visitor Visitor) {
+	if node != nil {
+		node.Left.InOrder(visitor)
+		visitor.Visit(node)
+		node.Right.InOrder(visitor)
+	}
+}
+
+type Counter struct {
+	Count int
+}
+
+func (counter *Counter) Visit(*Node) {
+	counter.Count++
+}
+
+type Summer struct {
+	Sum int
+}
+
+func (summer *Summer) Visit(node *Node) {
+	summer.Sum += node.Value
 }
--- avl_test.go
+++ avl_test.go
@@ -1,7 +1,45 @@
 package avl
 
-import "testing"
+import (
+	"testing"
+	"time"
+	"math/rand"
+)
 
-func TestExample(t *testing.T) {
-	t.Fail()
+func TestSummer(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+
+	tree := &Node{nil, 500, nil}
+
+	expected := 500
+
+	for i := 0; i < 10; i++ {
+		value := rand.Int() % 1000
+		tree.Insert(value)
+		expected += value
+	}
+
+	summer := &Summer{}
+	tree.InOrder(summer)
+
+	if summer.Sum != expected {
+		t.Error("expected:",expected,", actual:",summer.Sum)
+	}
+}
+
+func TestCounter(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+
+	tree := &Node{nil, 500, nil}
+
+	for i := 0; i < 10; i++ {
+		tree.Insert(rand.Int()%1000)
+	}
+
+	counter := &Counter{}
+	tree.InOrder(counter)
+
+	if counter.Count != 11 {
+		t.Error("expected:",11,", actual:",counter.Count)
+	}
 }
